b873bd8e2d0414b772fd8103e4068ca498746ba3
Brandon Garibaldi <bmg.oak@gmail.com>
2016-10-28 13:15:33 -0700

    added ruby puts questions for name, age, year born, garlic preference, insurance and gets.chomp input for them

---
+++ ruby/vampires.rb
@@ -0,0 +1,10 @@
+puts "what is your name?"
+name = gets.chomp
+puts "what is your age?"
+age = gest.chomp
+puts "what year were you born?"
+birth_year = gets.chomp
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+garlic_bread? = gets.chomp
+puts "Would you like to enroll in the company’s health insurance?"
+insurance = gets.chomp
