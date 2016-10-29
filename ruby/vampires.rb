7f41b18ad621105ea1fa15f1dcae5ecaf8d1afff
Brandon Garibaldi <bmg.oak@gmail.com>
2016-10-28 20:04:04 -0700

    changed original else  statement to elsif, added elsif to test for accurate age/birth year and (garlic hate and need for insurance)

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,4 +1,4 @@
-20161028 1808
+# 20161028 1808
 current_year = Time.new.year
 
 puts "what is your name?"
@@ -12,8 +12,43 @@ garlic_bread = gets.chomp
 puts "Would you like to enroll in the company’s health insurance?"
 insurance = gets.chomp
 
-if (current_year - birth_year) == age && garlic_bread = "yes"
+if (current_year - birth_year == age) && (garlic_bread != "no")
   puts("Probably not a vampire.")
-  else
-  	puts("Probably a vampire")
-end  
+    elsif (current_year - birth_year != age) && (garlic_bread != "yes" || insurance != "yes")
+      puts("Probably a vampire")
+        else
+  	  puts("Results incunclusive")
+end  	  
+# working on this: 1858
+
+# if (current_year - birth_year != age) && garlic_bread = "no"
+#   puts("Probably a vampire")
+#   else
+#   	puts("Results incunclusive")
+# end    
+
+# if (current_year - birth_year != age) && (garlic_bread != "yes" || insurance != "yes")
+#   puts("Probably a vampire")
+#   else
+#   	puts("Results incunclusive")
+
+
+#  working on this:  1940
+#  current_year = Time.new.year
+
+# #puts "what is your name?"
+# #name = gets.chomp
+# puts "what is your age?"
+# age = gets.chomp.to_i 
+# puts "what year were you born?"
+# birth_year = gets.chomp.to_i
+# puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+# garlic_bread = gets.chomp
+# puts "Would you like to enroll in the company’s health insurance?"
+# insurance = gets.chomp
+
+# if (current_year - birth_year != age) && (garlic_bread != "yes" || insurance != "yes")
+#   puts("Probably a vampire")
+#   else
+#   	puts("Results incunclusive")
+# end    
