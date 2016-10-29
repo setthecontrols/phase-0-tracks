118b001f7308c3b2836c555dd50dae7743c02ba4
Brandon Garibaldi <bmg.oak@gmail.com>
2016-10-28 18:14:33 -0700

    added current_year = Time.new.year and boolean if satement age right && willing to eat garlic bread - result prob not a vamp

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,10 +1,19 @@
+20161028 1808
+current_year = Time.new.year
+
 puts "what is your name?"
 name = gets.chomp
 puts "what is your age?"
-age = gest.chomp
+age = gets.to_i 
 puts "what year were you born?"
-birth_year = gets.chomp
+birth_year = gets.to_i
 puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-garlic_bread? = gets.chomp
+garlic_bread = gets.chomp
 puts "Would you like to enroll in the company’s health insurance?"
 insurance = gets.chomp
+
+if (current_year - birth_year) == age && garlic_bread = "yes"
+  puts("Probably not a vampire.")
+  else
+  	puts("Probably a vampire")
+end  
