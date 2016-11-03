215ab11ba98e26dbcfd41c1d6b2fba9fddffb98d
Brandon Garibaldi <bmg.oak@gmail.com>
2016-11-02 22:01:00 -0700

    add all methods described in doc and any necessary arguments

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,51 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+puts "iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+# "zom".insert(2, "o")
 # => “zoom”
 
-# "enhance".<???>
+puts "enhance".center(20)
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+puts "Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
+puts "the usual".concat(" suspects")
 #=> "the usual suspects"
 
-# " suspects".<???>
+puts " suspects".prepend("the usual")
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+puts "The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+puts "The mystery of the missing first letter".slice!(0)
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+puts "Elementary,    my   dear        Watson!".squeeze(" ")
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+puts "z".codepoints
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
+# 122 is the ASCII code for "z".
 
-# "How many times does the letter 'a' appear in this string?".<???>
+puts "How many times does the letter 'a' appear in this string?".count "a"
 # => 4
+
+# returns:
+
+# phase-0-tracks :> ruby ruby/method_detective.rb 
+# InVeStIgAtIoN
+#       enhance       
+# STOP! YOU’RE UNDER ARREST!
+# the usual suspects
+# the usual suspects
+# The case of the disappearing last lette
+# T
+# Elementary, my dear Watson!
+# 122
+# 4
