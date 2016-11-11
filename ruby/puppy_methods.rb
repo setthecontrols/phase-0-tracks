7ec28d5f1e8326c892813358cd265622bedd263b
Brandon Garibaldi <bmg.oak@gmail.com>
2016-11-11 08:40:08 -0800

    added Code class, 2 instance methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -25,6 +25,12 @@ def sleep(int)
 	puts "Wake me up in #{int} hours.  It's nap time!"
 end
 
+def initialize
+	puts "-"*10
+	puts "Initializng new Puppy instance..."
+	puts "-"*10
+end
+
 ollie = Puppy.new
 
 ollie.fetch("ball")
@@ -32,4 +38,28 @@ ollie.speak(7)
 ollie.roll_over
 ollie.dog_years(4)
 ollie.sleep(3)
+end
+
+# create class
+class Code
+# class write 2 instance methods and an initialize method
+def initialize
+	puts "-"*10
+	puts "Initializng new Code instance..."
+	puts "-"*10
+end
+
+def failure
+	puts "What'd I do wrong?"
+end
+
+def success
+	puts "Woo hoo!  It worked!"
+end
+week6 = Code.new
+week6.failure
+week6.success
+# Use a loop to make 50 instances of your class.
+# Modify your loop so that it stores all of the instances in a data structure.
+# Iterate over that data structure using .each and call the instance methods you wrote on each instance. So if you wrote a Gymnast class, this is where you'd call .flip and .jump on each of your instances of Gymnast
 end
