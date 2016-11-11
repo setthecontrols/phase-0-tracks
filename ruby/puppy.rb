b977fbf80115699960ce53c90c522dd7b76c690e
Brandon Garibaldi <bmg.oak@gmail.com>
2016-11-10 16:44:48 -0800

    added theoretical puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,18 @@
+## puppy class
+
+SPECIES-----------------
+Puppis Rascaleus
+
+CHARACTERISTICS---------
+Color: 
+Temprament:
+Size:
+Level of activity:
+Appetite:
+
+BEHAVIOR----------------
+Play:
+Eat:
+Potty:
+Destroy:
+Learn:
