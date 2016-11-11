c6f7c7eddb8b6fbf495a836df8f27664fb5273ed
Brandon Garibaldi <bmg.oak@gmail.com>
2016-11-11 08:06:23 -0800

    added speak and roll_over methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,8 +5,21 @@ class Puppy
     toy
   end
 
+ def speak(int)
+  i = 0
+  while i < int
+  	puts "Woof!"
+  	i += 1
+  end
+end
+
+def roll_over
+	puts "*rolls over*"
 end
 
 ollie = Puppy.new
 
 ollie.fetch("ball")
+ollie.speak(7)
+ollie.roll_over
+end
