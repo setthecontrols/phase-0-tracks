0480810d84fa0c7048268ad16f899a47c3c3ae4b
Brandon Garibaldi <bmg.oak@gmail.com>
2016-11-11 15:22:00 -0800

    add santa class, defined spak method

---
+++ ruby/santa.rb
@@ -0,0 +1,51 @@
+class Santa
+
+  def speak
+    puts "Ho, ho, ho! Haaaappy holidays!"
+  end
+end
+
+speak
+
+
+# write new class - babies
+# class Babies
+# 	# write 2 instance methods
+#   #potty
+#   def potty(int)
+#   	if int == 1
+#   		puts "Looks like we've got a number 1!"
+#   	else int == 2
+#   		puts "Uh, oh. It's a stinker!"
+#   	end
+#  end
+
+#   def sleep(bool)
+#     if bool == true
+#   	  puts "Then I can get some work done."
+#     else 
+#       puts "Then I'm changing diapers!"
+#   	end
+#   end
+#   def initialize
+# 	puts "-"*10
+# 	puts "Initializng new Babies instance..."
+# 	puts "-"*10
+# end
+
+# end
+# #create loop to make 50 instances
+# #create structure to store instances in
+# instance_arr = []
+# 50.times do | number |
+#   marlo = Babies.new
+#   instance_arr << Babies.new
+#   p instance_arr
+# end
+# instance_arr.each do | inst_num |
+#   inst_num.potty(1)
+#   inst_num.sleep(true)
+# end
+
+#   # marlo.potty(1)
+#   # marlo.sleep(true)
