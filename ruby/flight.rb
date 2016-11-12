0409f65e93b2b03ceb6f4eae00cdd1b6bf53ad78
Brandon Garibaldi <bmg.oak@gmail.com>
2016-11-12 15:34:53 -0800

    added Flight module to Plane and Bird clases and driver code for take_off method.

--- ruby/flight.rb
+++ ruby/flight.rb
@@ -1,14 +1,32 @@
 
-module Flight
-  def take_off(altitued)
-  	puts "Taking off and ascending until reaching #{altitued}."
-  end
-end
+# module Flight
+#   def take_off(altitued)
+#   	puts "Taking off and ascending until reaching #{altitued}."
+#   end
+# end
 
 
 class Bird
+
+  module Flight
+    def take_off(altitued)
+	  puts "Taking off and ascending until reaching #{altitued}."
+	end
+  end
 end
 
 class Plane
+
+  module Flight
+    def take_off(altitued)
+	  puts "Taking off and ascending until reaching #{altitued}."
+	end
+  end
 end
+
+bird = Bird.new
+bird.take_off(800)
+
+plane = Plane.new
+plane.take_off(30000)
 
