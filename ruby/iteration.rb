0e4e5ccdcc8f3c6a99703d17605a345c12a39def
Brandon Garibaldi <bmg.oak@gmail.com>
2016-11-25 18:12:59 -0800

    initialized hash storing  drummers and drums they played

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -8,10 +8,22 @@ dogs {|name,age| puts "Why thank you.  His name is #{name} and is #{age} years o
 
 
 drummers_drums_used = {
-	"Buddy Rich" => "Ludwig",
-	"Keith Moon" => "Premier",
-	"Mitch Mitchell" => "Ludwig"
-	"John Bonham" => "Ludwig",
-	"Louie Bellson" => "Rogers"
+	"Buddy Rich" => ["Ludwig"],
+	"Keith Moon" => ["Premier"],
+	"Mitch Mitchell" => ["Ludwig"],
+	"John Bonham" => ["Ludwig"],
+	"Louie Bellson" => ["Rogers"]
 }
 
+
+
+drummers_drums_used.each do |drummer, drums |
+  puts "#{drummer} played #{drums} drums."
+end
+  puts "But Louie also played Slingerland, so let's add that now:"
+drummers_drums_used.each do |drummer, drums|
+  if drummer == "Louie Bellson"
+    drummers_drums_used["Louie Bellson"][1] = "Slingerland"
+  end
+end
+drummers_drums_used
