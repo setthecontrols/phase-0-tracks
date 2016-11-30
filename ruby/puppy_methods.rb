8d5319524b36daecc132c458c26a4e5b968a9eea
Brandon Garibaldi <bmg.oak@gmail.com>
2016-11-30 00:16:00 -0800

    added dirt riding class, two methods and created, stored and called instance methods on 50 instances

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,37 +1,77 @@
-class Puppy
+class DirtRiding
 
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
+  def method_name
+    puts "**********Initializing Dirt Riding Method**********"
   end
 
-  def speak(int)
-    puts "Woof! "*int
-  end
-
-  def roll_over
-    puts "*rolls over*"
-  end
-
-  def dog_years(int)
-    puts int*7
-  end
-
-  def use_toilett(int)
-    if int == 1
-      puts "If it's yellow, let it mellow."
-    elsif int == 2
-      puts "if it's brown, flush it down."
-    else 
-      puts "I'm not familiar with that one."
+  def hill_climbing(bool)
+    if bool == true
+      "Going up hill!"
+    else bool == false
+      "Let's find a hill to climb!"
     end
+        
+  end
+
+  def trail_riding
+    "Riding that trail!"
   end
 
 end
 
-puppy = Puppy.new
-puppy.fetch("stuffed animal")
-puppy.speak(8)
-puppy.roll_over
-puppy.dog_years(4)
-puppy.use_toilett(5)
+instances = []
+50.times do | i |
+ instances << riding = DirtRiding.new
+end
+instances.each do | instance |
+  
+  puts instance.hill_climbing(true)
+  puts instance.trail_riding
+end
+puts " "
+
+
+
+
+# class Puppy
+
+#   def initialize
+#     puts "**********INITIALIZING NEW PUPPY CLASS**********"
+#   end
+
+#   def fetch(toy)
+#     puts "I brought back the #{toy}!"
+#     toy
+#   end
+
+#   def speak(int)
+#     puts "Woof! "*int
+#   end
+
+#   def roll_over
+#     puts "*rolls over*"
+#   end
+
+#   def dog_years(int)
+#     puts int*7
+#   end
+
+#   def use_toilett(int)
+#     if int == 1
+#       puts "If it's yellow, let it mellow."
+#     elsif int == 2
+#       puts "if it's brown, flush it down."
+#     else 
+#       puts "I'm not familiar with that one."
+#     end
+#   end
+
+# end
+
+# puppy = Puppy.new
+# puppy.fetch("stuffed animal")
+# puppy.speak(8)
+# puppy.roll_over
+# puppy.dog_years(4)
+# puppy.use_toilett(5)
+
