837087b2ad2075ffc8941fd5243f27a470f918ec
Brandon Garibaldi <bmg.oak@gmail.com>
2016-11-30 00:16:34 -0800

    uncommented puppy class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -33,45 +33,45 @@ puts " "
 
 
 
-# class Puppy
+class Puppy
 
-#   def initialize
-#     puts "**********INITIALIZING NEW PUPPY CLASS**********"
-#   end
+  def initialize
+    puts "**********INITIALIZING NEW PUPPY CLASS**********"
+  end
 
-#   def fetch(toy)
-#     puts "I brought back the #{toy}!"
-#     toy
-#   end
+  def fetch(toy)
+    puts "I brought back the #{toy}!"
+    toy
+  end
 
-#   def speak(int)
-#     puts "Woof! "*int
-#   end
+  def speak(int)
+    puts "Woof! "*int
+  end
 
-#   def roll_over
-#     puts "*rolls over*"
-#   end
+  def roll_over
+    puts "*rolls over*"
+  end
 
-#   def dog_years(int)
-#     puts int*7
-#   end
+  def dog_years(int)
+    puts int*7
+  end
 
-#   def use_toilett(int)
-#     if int == 1
-#       puts "If it's yellow, let it mellow."
-#     elsif int == 2
-#       puts "if it's brown, flush it down."
-#     else 
-#       puts "I'm not familiar with that one."
-#     end
-#   end
+  def use_toilett(int)
+    if int == 1
+      puts "If it's yellow, let it mellow."
+    elsif int == 2
+      puts "if it's brown, flush it down."
+    else 
+      puts "I'm not familiar with that one."
+    end
+  end
 
-# end
+end
 
-# puppy = Puppy.new
-# puppy.fetch("stuffed animal")
-# puppy.speak(8)
-# puppy.roll_over
-# puppy.dog_years(4)
-# puppy.use_toilett(5)
+puppy = Puppy.new
+puppy.fetch("stuffed animal")
+puppy.speak(8)
+puppy.roll_over
+puppy.dog_years(4)
+puppy.use_toilett(5)
 
