fbce0b184806c423b18dc18358f605f2611194e1
Brandon Garibaldi <bmg.oak@gmail.com>
2016-11-29 23:36:08 -0800

    added dog years and use toilett methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -13,9 +13,25 @@ class Puppy
     puts "*rolls over*"
   end
 
+  def dog_years(int)
+    puts int*7
+  end
+
+  def use_toilett(int)
+    if int == 1
+      puts "If it's yellow, let it mellow."
+    elsif int == 2
+      puts "if it's brown, flush it down."
+    else 
+      puts "I'm not familiar with that one."
+    end
+  end
+
 end
 
 puppy = Puppy.new
 puppy.fetch("stuffed animal")
 puppy.speak(8)
 puppy.roll_over
+puppy.dog_years(4)
+puppy.use_toilett(5)
