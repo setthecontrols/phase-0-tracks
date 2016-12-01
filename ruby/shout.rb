c021cf7b83f1a1366220a61e1b5051d24e34e79f
Brandon Garibaldi <bmg.oak@gmail.com>
2016-11-30 22:51:53 -0800

    edit Shout to be mixin, added classes monkey and cockatoo and added driver code to call module methods on class instcs

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,8 +1,52 @@
 module Shout 
-  def self.yell_angrily(words)
+
+  def yell_angrily(words)
     words + "!!!!" + " :("
   end
-  def yelling_happily(word)
-    words + "in a really happy way!!!!"
+  
+  def yelling_happily(words)
+    words + " in a really happy way!!!!"
   end
+
 end
+
+
+class Monkeys
+  include Shout
+end
+
+class Cockatoo
+  include Shout
+
+end
+
+
+
+mono = Monkeys.new
+puts mono.yell_angrily("in monkey:  why am I in this f*cking cage???")
+puts mono.yelling_happily("in monkey:  I guess the danger of being eating has gone down by, like 100%")
+
+c_too = Cockatoo.new
+puts c_too.yell_angrily("in cockatoo:  why am I in this f*cking cage???")
+puts c_too.yelling_happily("in cockatoo:  I guess the danger of being eating has gone down by, like 100%")
+
+
+
+
+
+
+
+# module Shout 
+
+#   def self.yell_angrily(words)
+#     words + "!!!!" + " :("
+#   end
+  
+#   def self.yelling_happily(words)
+#     words + " in a really happy way!!!!"
+#   end
+
+# end
+
+# puts Shout.yell_angrily("angry things")
+# puts Shout.yelling_happily("super happy stuff")
