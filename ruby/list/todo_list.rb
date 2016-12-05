a29b230e5b59841ddd5d54263b989a3eb22852ff
Brandon Garibaldi <bmg.oak@gmail.com>
2016-12-04 23:05:10 -0800

    added all code sent from driver in pair

--- ruby/list/todo_list.rb
+++ ruby/list/todo_list.rb
@@ -1,24 +1,22 @@
-
 class TodoList
-	
-	def initialize(list)
-		@list = list
-	end
+  def initialize(array)
+    @list = array
+  end
 
-	def get_items
-		@list	
-	end
+  def get_items
+    @list
+  end
 
-	def add_item(item)
-		@list << item
-	end
+  def add_item(element)
+    @list << element
+  end
 
-	def delete_item(item)
-		@list.delete item
-	end
+  def delete_item(item)
+    @list.delete_at(@list.index(item))
+  end
 
-	def get_item(i)
-		@list[i]
-	end
-
+  def get_item(i)
+   @list[i]
+  end
 end
+
