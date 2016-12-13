f4e99448b780c5942ad0c119fd47cc6ea414d3a6
Brandon Garibaldi <bmg.oak@gmail.com>
2016-12-12 22:58:31 -0800

    added car constructor function

--- js/data_structures.js
+++ js/data_structures.js
@@ -2,7 +2,7 @@ var colors = ["orange", "white", "pink", "black"]
 var names = ["goblin", "stripes", "floyd", "sabbath"]
 
 
-function add_name(arr, color){
+function add_color(arr, color){
 	arr.push(color);
 console.log(arr);
 }
@@ -24,3 +24,23 @@ function add_names_and_colors(color, name){
 	console.log(horses);
 }
 add_names_and_colors(colors, names)
+
+
+
+function Car(fuleType, year, isWorking){
+	console.log("Here's a car:", this);
+	this.fuleType = fuleType;
+	this.year = year;
+	this.isWorking = isWorking;
+
+	this.reverse = function() {console.log("Backing up, over here!");};
+
+	console.log("CAR INITIALIZED")
+}
+
+console.log("Let's put a car together!")
+var ourCar_01 = new Car("coal", 1892, false)
+console.log("ourCar_01")
+console.log("Our car can reverse!")
+ourCar_01.reverse()
+console.log("---------")
